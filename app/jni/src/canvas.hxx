504256a3822cb4fd8840fe12c472d51c5cc6a00e
SlothPlayer2241 <slothplayer12341@gmail.com>
2025-11-09 06:01:37 -0800

    Add background texture and note counting methods

--- app/jni/src/canvas.hxx
+++ app/jni/src/canvas.hxx
@@ -25,6 +25,8 @@ public:
     int   TH, WinW, WinH;
     bool  KeyPress[128];
     unsigned int KeyColor[128];
+    float KeyAlpha[128];
+    bool KeyJustHit[128];
 
     Canvas(); 
     ~Canvas();
@@ -32,16 +34,43 @@ public:
     void canvas_clear();
 
     void DrawKeyBoard();
+    void DrawVerticalLines();
+    void DrawBackgroundImage();
 
     void Note(int k, int yb, int ye, unsigned int c);
+    unsigned int GetNoteColor(int note_number);
+    bool LoadBackgroundImage(const std::string& image_path);
+    bool IsNoteInRange(int note_number);
+    void OptimizeRendering();
+    float GetFrameRate();
+    
+    // Note counting methods
+    bool isNoteCounted(int key) const { return NoteCounted[key]; }
+    void setNoteCounted(int key, bool value) { NoteCounted[key] = value; }
+    void incrementNoteCount() { note_hits_count++; }
+    unsigned int getNoteHitsCount() const { return note_hits_count; }
 
 private:
 
-    SDL_Texture *Bk0, *Bk1, *Wk, *note;
+    SDL_Texture *Bk0, *Bk1, *Wk, *note, *background_texture;
     SDL_Surface *colors;
 
     int BkeyW, BkeyH, WkeyW, WkeyH;
     int TW, TX[11], KeyX[128];
+    
+    // Performance monitoring
+    float frame_rate;
+    float last_frame_time;
+    int frame_count;
+    float fps_update_timer;
+
+    // Note counter: total notes hit since app start
+    int note_hits_count;
+    bool NoteCounted[128];
+    unsigned int GetNoteCount();
+    bool isNoteCounted(int key) const { return NoteCounted[key]; }
+    void setNoteCounted(int key, bool counted) { NoteCounted[key] = counted; }
+    void incrementNoteCount() { note_hits_count++; }
 
     int scale(int x) const;
 
